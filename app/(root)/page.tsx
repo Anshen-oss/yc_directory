578cf7dafdade42bf12874ddc42e47c8b68e0332
Anshen-oss <plouisin@gmail.com.com>
2024-11-09 17:18:51 +0100

    Formulaire de recherche

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: {searchParams: Promise<{ query?: string}> }) {
+    const query = (await searchParams).query;
+
   return (
     <>
         <section className="pink_container">
@@ -13,6 +15,7 @@ export default function Home() {
                 Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
                 Competitions.
             </p>
+            <SearchForm query={query} />
         </section>
 
     </>

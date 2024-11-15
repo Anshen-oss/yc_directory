9c170178a6e91c21e970a60d931671a529bd8db7
Anshen-oss <plouisin@gmail.com.com>
2024-11-15 10:09:04 +0100

    Feching Data and >Type Safety

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,55 +1,48 @@
 import { cn, formatDate } from "@/lib/utils";
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
 
-
-export default async function Home({ searchParams }: {searchParams: Promise<{ query?: string}> }) {
-    const query = (await searchParams).query;
-
-    const posts = [{
-        _createdAt: new Date(),
-        views: 55,
-        author: { _id: 1, name: "Anshen Kondjah" },
-        _id: 1,
-        description: 'This a description',
-        image: "https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        category: "Books",
-        title: "My prefered books",
-    },
-    ];
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
-        <section className="pink_container">
-            <h1 className="heading">
-                Pitch Your Startup, <br/>
-                Connect With Entrepreneurs
-            </h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
 
-            <p className="sub-heading !max-w-3xl">
-                Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
-                Competitions.
-            </p>
-            <SearchForm query={query} />
-        </section>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
 
-        <section className="section_container">
-            <p className="text-30-semibold">
-                {query ? `Search results for "${query}"` : "All Startups"}
-            </p>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
 
-            <ul className="mt-7 card_grid">
-                {posts?.length > 0 ? (
-                    posts.map((post: StartupCardType, index: number) => (
-                        <StartupCard key={post?._id} post={post} />
-                    ))
-                ):(
-                    <p className="no-results">No startups found</p>
-                )}
-            </ul>
-        </section>
-
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
